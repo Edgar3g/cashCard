0319b820694efaf7b43cf85ded94ac1a6a8d0c7c
Edgar A. Dikenge <dikengeofficial@gmail.com>
2024-02-25 01:29:10 +0100

    feat: run the first test

---
+++ src/test/java/ao/dikenge/cashCard/CashCardJsonTest.java
@@ -0,0 +1,14 @@
+package ao.dikenge.cashCard;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class CashCardJsonTest {
+
+    @Test
+    void myFistTest() {
+        assertThat(1).isEqualTo(1);
+    }
+
+}
